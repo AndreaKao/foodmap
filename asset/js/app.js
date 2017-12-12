192aa1f9bfb18cc5c3905eb64cb5bd2b86ea9914
agustinaVal <titivalenti24@gmail.com>
2017-12-12 13:05:41 -0300

    subiendo

---
+++ asset/js/app.js
@@ -0,0 +1,19 @@
+/*
+$( "progress" ).click(function() {
+  function complete() {
+    $( "<div>" ).text( this.id ).appendTo( "#log" );
+  }
+  $( "progress" ).fadeOut( 1600, "linear", complete );
+});
+ 
+ */
+ /*
+$( "#btn2" ).click(function() {
+  $( "div" ).show();
+  $( "#log" ).empty();
+});
+*/
+$(document).ready(function(){
+    $('.container1').fadeIn(5000).fadeOut(2500);
+    
+});
