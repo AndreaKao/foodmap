1ade8a393068cb21629ba104ed72557ea062daff
andreakao <andreaperez.gz@gmail.com>
2017-12-13 06:08:25 -0300

    otros nuevos cambios

--- asset/js/app.js
+++ asset/js/app.js
@@ -14,6 +14,33 @@ $( "#btn2" ).click(function() {
 });
 */
 $(document).ready(function(){
-    $('.container1').fadeIn(5000).fadeOut(2500);
-    
+   $('.container1').fadeOut(3300);
 });
+
+$(document).ready(function(){
+   $('.container2').delay(3100).fadeIn("slow");
+});
+
+$(document).bind("mobileinit", function(){
+  $.mobile.listview.prototype.options.filterPlaceholder = "Texto del PlaceHolder Cambiado Por Código";    
+});
+
+
+
+/*
+$(document).ready(function(){
+	$('.container2').fadeIn(3600);
+});
+
+/*
+
+		$(".container2").click(function(){
+			$('#target').show(3000);
+			$('.target').show("slow");
+		 });
+		$("#ocultar").click(function(){
+			$('#target').hide(3000);
+			$('.target').hide("fast");
+		 });
+	});
+	*/
